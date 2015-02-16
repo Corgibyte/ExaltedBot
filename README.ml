3cc923f9ffa6fbef1ff047929a883b91c285e229
Hannah Young <hannah@corgibyte.com>
2015-02-16 12:26:45 -0800

    Added README

---
+++ README.ml
@@ -0,0 +1,14 @@
+An IRC bot forked from the wonderful [PircBotX](http://code.google.com/p/pircbotx/)
+to perform mechanical tasks for the Exalted 3rd Edition roleplaying game. Currently does:
+
+*Nothing
+
+TODO:
+
+*Parse messages from channel
+*Implement character skills
+*Implement Character objects
+*Implement builder for characters
+*Use bot to register Characters
+*Implement dice roller
+
